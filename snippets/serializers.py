7a5c072020b5dacff608a4bdf9b75eaa96a22d16
Ananyobrata Pal <ananyo141@gmail.com>
2023-02-24 21:11:31 +0530

    add hyperlinking and pagination

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,19 +3,29 @@ from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
 
     class Meta:
         model = Snippet
-        fields = ("id", "owner", "title", "code", "linenos", "language", "style")
+        fields = (
+            "url",
+            "id",
+            "owner",
+            "highlighted",
+            "title",
+            "code",
+            "linenos",
+            "language",
+            "style",
+        )
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name="snippet-detail", read_only=True
     )
 
     class Meta:
         model = User
-        fields = ("id", "username", "snippets")
+        fields = ("url", "id", "username", "snippets")
